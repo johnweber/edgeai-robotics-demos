74e381d40f1a137d439f15245af9ff1b43f572db
John Weber <john.weber@technexion.com>
2024-02-15 23:46:44 -0600

    python:samples:simple_movement: Use a loop and print the commands

--- python/samples/simple_movement.py
+++ python/samples/simple_movement.py
@@ -2,28 +2,37 @@ import sys
 import time
 from ddcontroller import *
 
+def print_motion(linvel, angvel):
+    print(f'Motion: Linear={linvel:2f}, Angular={angvel:2f}')
+
 config = '/opt/robot/edgeai-robotics-demos/python/common/ddcontroller/config/rovybot_config.yaml'
+
+velfactors = [[1.0, 0.0],
+            [0.5, 1.0],
+            [0.0, 1.0],
+            [0.0, 0.0],
+            [0.0, -1.0],
+            [-0.5, -1.0],
+            [-1.0, 0.0]]
 
 try:
     robot = DDRobot(config_path=config, debug=True)
-except Exception as e:
-    print("Exception: {0}".format(e))
-    sys.exit(2)
 
-robot.set_motion([1.0*robot.max_linear_velocity, 0.0*robot.max_angular_velocity])
-time.sleep(5)
-robot.set_motion([1.0*robot.max_linear_velocity, 1.0*robot.max_angular_velocity])
-time.sleep(5)
-robot.set_motion([0.0*robot.max_linear_velocity, 1.0*robot.max_angular_velocity])
-time.sleep(5)
-robot.set_motion([0.0*robot.max_linear_velocity, 0.0*robot.max_angular_velocity])
-time.sleep(5)
-robot.set_motion([-1.0*robot.max_linear_velocity, 0.0*robot.max_angular_velocity])
-time.sleep(5)
-robot.set_motion([-1.0*robot.max_linear_velocity, -1.0*robot.max_angular_velocity])
-time.sleep(5)
-robot.set_motion([0.0*robot.max_linear_velocity, -1.0*robot.max_angular_velocity])
-time.sleep(5)
-robot.stop()
+    for velfactor in velfactors:
+        linvel = velfactor[0]*robot.max_linear_velocity
+        angvel = velfactor[1]*robot.max_angular_velocity
+        print_motion(linvel, angvel)
+        robot.set_motion([linvel, angvel])
+        time.sleep(5)
+
+    robot.stop()
+
+except KeyboardInterrupt:
+    print("Keyboard interrrupt")
+    robot.stop()
+
 del robot
+
+print("exiting")
+sys.exit(0)
 
